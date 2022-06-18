3879209cf34ff95944a219fe4be220cd8b6def03
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2022-06-18 19:41:45 +0200

    Bug fixes + added comments
    Bug fixes + added comments

--- Main.py
+++ Main.py
@@ -1,68 +1,70 @@
-# The Evil Puzwick virus
-
+# imports
 from tkinter import *
+from sys import exit
+from os import getenv
 from Utils import randomize_position
-import sys
-import os
 
+# global variables
 WIDTH = 350
 HEIGHT = 148
-BG_COLOR = "#8a8281"
-UNLOCK_CODE = "yX4eaY@v"
-ABSOLUTE_PATH = os.getenv('APPDATA')
+BG_COLOR = '#a19594'
+UNLOCK_CODE = 'yX4eaY@v'
+ABSOLUTE_PATH = getenv('APPDATA')
 
 
+# main function of the script
 def init():
 
+    # initialize the window
     win = Tk()
     win.resizable(0, 0)
-    win.protocol("WM_DELETE_WINDOW", init)  # opens a new window when the user tries to close it
+    win.protocol('WM_DELETE_WINDOW', init)  # opens a new window when the user tries to close it
     win.attributes('-topmost', True)  # setting the window to be always displayed on top
-
-    coordinates = randomize_position(win, WIDTH, HEIGHT)
-    win.geometry(str(WIDTH) + 'x' + str(HEIGHT) + '+' + coordinates[0] + '+' + coordinates[1])
-
-    win.title("The Evil Puzwick")
-    icon = PhotoImage(file=ABSOLUTE_PATH + '\TheEvilPuzwick\puzwick_ico.png', master=win)
-    win.iconphoto(True, icon)
+    win.title('Ooops...ho infettato il tuo pc!')
     win.config(bg=BG_COLOR)
 
-    # Widgets
+    coordinates = randomize_position(win, WIDTH, HEIGHT)  # from Utils.py
+    win.geometry(str(WIDTH) + 'x' + str(HEIGHT) + '+' + coordinates[0] + '+' + coordinates[1])
 
-    def submit():
+    icon = PhotoImage(file=ABSOLUTE_PATH + '\TheEvilPuzwick\puzwick_ico.png', master=win)
+    win.iconphoto(True, icon)
+
+    # button event
+    def button_submit():
         text = entry.get()
         if text == UNLOCK_CODE:
-            sys.exit()
+            exit()
         else:
             entry.delete(0, END)
             init()
 
-    def binded_submit(self):
+    def enter_submit(self):
         text = entry.get()
         if text == UNLOCK_CODE:
-            sys.exit()
+            exit()
         else:
             entry.delete(0, END)
             init()
 
+    # widgets
     image = PhotoImage(file=ABSOLUTE_PATH + '\TheEvilPuzwick\puzwick.png', master=win)
     image_label = Label(win, image=image)
-    text_label = Label(win, text="Heilà!!! Carino questo computer, penso\n"
-                                 "che mi stabilirò qui! Ti consiglio di non\n"
-                                 "provare a sbarazzarti di me, heheh!\n"
-                                 "L'unica via di fuga è inserire il codice\n"
-                                 "corretto nella casella, buona sfortuna!", font=('Arial', 8), bg=BG_COLOR)
+    text_label = Label(win, text='Heilà!!! Carino questo computer, penso\n'
+                                 'che mi stabilirò qui! Ti consiglio di non\n'
+                                 'provare a sbarazzarti di me, heheh!\n'
+                                 'L\'unica via di fuga è inserire il codice\n'
+                                 'corretto nella casella, buona sfortuna!', font=('Arial', 8), bg=BG_COLOR)
     entry = Entry(win, relief='groove')
-    button = Button(win, text="Prova!", command=submit)
+    button = Button(win, text='Prova!', command=button_submit)
 
     image_label.place(x=0, y=-2)
     text_label.place(x=148, y=5)
     entry.place(x=182, y=85)
     button.place(x=222, y=115)
 
-    win.bind('<Return>', binded_submit)  # Binding the "Enter" key to the button
+    win.bind('<Return>', enter_submit)  # binding the "Enter" key to the button
 
-    win.mainloop()
+    win.mainloop()  # displaying the window
 
 
-init()
+init()  # main function call
--- Utils.py
+++ Utils.py
@@ -1,6 +1,7 @@
 from random import randint
 
 
+# function to place the window in a random position of the screen
 def randomize_position(win, width, height):
     x = randint(0, win.winfo_screenwidth() - width)
     y = randint(0, win.winfo_screenheight() - height)
